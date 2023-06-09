b3a33f185287ad6a57f205e84f2258ad3b46e0d0
sonetyler <104440476+sonetyler@users.noreply.github.com>
2023-06-09 14:05:43 +0900

    Restart All

---
+++ Exercises/src/Ex1/DiceGame.java
@@ -0,0 +1,14 @@
+package Ex1;
+
+public class DiceGame {
+	public static void main(String[]args) {
+		System.out.println("rolling dice..");
+		int max = 6;
+		int min = 1;
+		int die1 = (int)Math.floor(Math.random()* (max - min) + min);
+		int die2 = (int)Math.floor(Math.random()* (max - min) + min);
+		System.out.println("Die 1: " + die1);
+		System.out.println("Die 2: " + die2);
+		System.out.println("Total value: " + (die1 + die2));
+	}
+}
