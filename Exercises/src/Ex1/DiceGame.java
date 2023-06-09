bea070cdd13d821dacf7d4d5da3aa379e0020cc6
sonetyler <104440476+sonetyler@users.noreply.github.com>
2023-06-09 14:20:55 +0900

    Won/Lost message added

--- Exercises/src/Ex1/DiceGame.java
+++ Exercises/src/Ex1/DiceGame.java
@@ -8,7 +8,14 @@ public class DiceGame {
 		int die1 = (int)Math.floor(Math.random()* (max - min) + min);
 		int die2 = (int)Math.floor(Math.random()* (max - min) + min);
 		System.out.println("Die 1: " + die1);
+		int total = die1 + die2;
 		System.out.println("Die 2: " + die2);
-		System.out.println("Total value: " + (die1 + die2));
+		System.out.println("Total value: " + (total));
+		if(total > 7) {
+			System.out.println("You Won!");
+		}
+		else {
+			System.out.println("You Lost.");
+		}
 	}
 }
